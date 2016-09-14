8ff4b7c4553097a3fd2f25e52c5e32e7cf4abe8a
SteveCoss <coss.31@osu.edu>
2016-09-14 09:18:34 -0400

    updated  .nc plot script to allow for alterations in writeAlt.m

--- NetCDF_Altimetry_Examples.m
+++ NetCDF_Altimetry_Examples.m
@@ -2,16 +2,22 @@
 
 clear all; close all;
 
-addpath Americas_Data % change this to the local directory where you have the altimetry data
+addpath 'C:\Users\Public\Documents\coss.31\Documents\MATH\Steves_final_Toolbox\MEaSUREsToolbox2016\AltimetryToolbox\Dataproduct' % change this to the local directory where you have the altimetry data
 
-% % Uncomment to look at all Americas -- warning opens ~300 Matlab figure windows
-% NorthAmerica={'Columbia','Mackenzie','StLawrence','Susquehanna', 'Yukon','Mississippi'};
-% SouthAmerica={'Amazon','Tocantins','Orinoco','SaoFrancisco','Uruguay','Magdalena','Parana','Oiapoque','Essequibo','Courantyne'};
-% Americas=[NorthAmerica SouthAmerica];
-% Rivers=Americas;
+% Uncomment to look at all Americas -- warning opens ~300 Matlab figure windows
+NorthAmerica={'Columbia','Mackenzie','StLawrence','Susquehanna', 'Yukon','Mississippi'};
+SouthAmerica={'Amazon','Tocantins','Orinoco','SaoFrancisco','Uruguay','Magdalena','Parana','Oiapoque','Essequibo','Courantyne'};
+Americas=[NorthAmerica SouthAmerica];
+Africa={'Congo','Nile','Niger','Zambezi'};
+Eurasia={'Amur','Anabar','Ayeyarwada','Kuloy','Ob','Menzen','Lena','Yenisei','Pechora','Pyasina','Khatanga','Olenyok' ...
+    ,'Indigirka','Kolyma','Anadyr','Yangtze','Mekong','Ganges','Brahmaputra','Indus','Volga'};
 
-Rivers={'Mackenzie'};
-% Rivers={'Mississippi'};
+SingleRiv={'StLawrence'};
+
+
+Rivers=SingleRiv ;
+TS=true
+SAVEPLOTS=false %save the plots
 
 %% map
 figure; lon=[]; lat=[];
@@ -20,8 +26,22 @@ for i=1:length(Rivers),
     FNAME1=[currRiv '_NetCDF_List.txt'];
     FID=fopen(FNAME1,'r');
     delimiter = ',';
-    startRow = 5;
-    formatSpec = '%s%[^\n\r]';
+    startRow = 3;
+ formatSpec = '%s';
+    dataArray = textscan(FID, formatSpec, 'Delimiter', delimiter, 'MultipleDelimsAsOne', true, 'HeaderLines' ,startRow-1, 'ReturnOnError', false);
+    fclose(FID);
+    VSLIST=dataArray{1};
+    if ~isempty(VSLIST)&& length(char(VSLIST(1)))<40;
+    for j=1:length(VSLIST),
+        FNAME2=[VSLIST{j} '.nc'];
+        lon=[lon ncread(FNAME2,'lon')];
+        lat=[lat ncread(FNAME2,'lat')];
+    end
+    else
+         FID=fopen(FNAME1,'r');
+    delimiter = ',';
+    startRow = 4;
+ formatSpec = '%s';
     dataArray = textscan(FID, formatSpec, 'Delimiter', delimiter, 'MultipleDelimsAsOne', true, 'HeaderLines' ,startRow-1, 'ReturnOnError', false);
     fclose(FID);
     VSLIST=dataArray{1};
@@ -30,9 +50,22 @@ for i=1:length(Rivers),
         lon=[lon ncread(FNAME2,'lon')];
         lat=[lat ncread(FNAME2,'lat')];
     end
+        
+    end
 end
 hold off;
+if length(lon)>1
 a=axis;
+ax=worldmap([min(lat) max(lat)],[min(lon) max(lon)]);
+%ax=worldmap('Eurasia')
+land=shaperead('landareas','UseGeoCoords',true);
+geoshow(ax,land,'FaceColor',[0.5 0.7 0.5]);
+rivers=shaperead('worldrivers','UseGeoCoords',true);
+geoshow(ax,rivers,'Color','blue');
+geoshow(lat,lon,'DisplayType','point')
+title('Selected virtual station locations')
+else 
+    a=axis;
 ax=worldmap([min(lat) max(lat)],[min(lon) max(lon)]);
 land=shaperead('landareas','UseGeoCoords',true);
 geoshow(ax,land,'FaceColor',[0.5 0.7 0.5]);
@@ -40,29 +73,53 @@ rivers=shaperead('worldrivers','UseGeoCoords',true);
 geoshow(ax,rivers,'Color','blue');
 geoshow(lat,lon,'DisplayType','point')
 title('Selected virtual station locations')
+   
+end
 
 %% timeseries
+if TS
 for i=1:length(Rivers)
     currRiv=Rivers{i};  
     FNAME1=[currRiv '_NetCDF_List.txt'];
     FID=fopen(FNAME1,'r');
     delimiter = ',';
-    startRow = 5;
+    startRow = 3;
     formatSpec = '%s%[^\n\r]';
     dataArray = textscan(FID, formatSpec, 'Delimiter', delimiter, 'MultipleDelimsAsOne', true, 'HeaderLines' ,startRow-1, 'ReturnOnError', false);
     fclose(FID);
     VSLIST=dataArray{1};
+    if isempty(VSLIST) ;
+        FID=fopen(FNAME1,'r');
+        delimiter = ',';
+        startRow = 4;
+        formatSpec = '%s%[^\n\r]';
+        dataArray = textscan(FID, formatSpec, 'Delimiter', delimiter, 'MultipleDelimsAsOne', true, 'HeaderLines' ,startRow-1, 'ReturnOnError', false);
+        fclose(FID);
+        VSLIST=dataArray{1};
+    end
+    if length(char(VSLIST(1))) >40;
+        FID=fopen(FNAME1,'r');
+        delimiter = ',';
+        startRow = 4;
+        formatSpec = '%s%[^\n\r]';
+        dataArray = textscan(FID, formatSpec, 'Delimiter', delimiter, 'MultipleDelimsAsOne', true, 'HeaderLines' ,startRow-1, 'ReturnOnError', false);
+        fclose(FID);
+        VSLIST=dataArray{1};
+        
+    end
 
     for j=1:length(VSLIST),
         figure;
         FNAME2=[VSLIST{j} '.nc'];
         t=ncread(FNAME2,'/Timeseries/time');
+        startdate=datenum(1900,1,1);
+        t=t+startdate;
         h=ncread(FNAME2,'/Timeseries/hwbar');
         var3=ncread(FNAME2,'ID');
         Flow_Dist=ncread(FNAME2,'Flow_Dist');
-        var4=ncread(FNAME2,'/Level3/lon');
-        var5=ncread(FNAME2,'/Level3/lat');
-        var6=ncread(FNAME2,'/Level3/h');
+        var4=ncread(FNAME2,'/Unprocessed GDR Data/lon');
+        var5=ncread(FNAME2,'/Unprocessed GDR Data/lat');
+        var6=ncread(FNAME2,'/Unprocessed GDR Data/h');
         var3=var3';
         gdat=(h>-1);
         t=t(gdat);  tv=datevec(t);
@@ -74,9 +131,14 @@ for i=1:length(Rivers)
         ti=t(1):t(end);
         hi=interp1(t,h,ti);
 
-        %if exists, grab ice filter info
-        tthaw=datevec(ncread(FNAME2,'/Filter/icethaw'));
-        tfreeze=datevec(ncread(FNAME2,'/Filter/icefreeze'));        
+%         if exists, %grab ice filter info
+tthaw=ncread(FNAME2,'/Filter/icethaw');
+tthaw=tthaw+startdate;
+tthaw=datevec(  tthaw);
+tfreeze=ncread(FNAME2,'/Filter/icefreeze');
+tfreeze=tfreeze+startdate;
+tfreeze=datevec(tfreeze);
+       
         if ~isempty(tthaw),
             tvi=datevec(ti);            
             for k=1:length(tvi),
@@ -114,6 +176,11 @@ for i=1:length(Rivers)
         else
             legend([han1 han2 ],'Altimeter data','Interpolated','Location','Best')
         end
+        if SAVEPLOTS
+            path=fullfile('C:\Users\coss.31\Documents\MATH\Steves_final_Toolbox\AltimetryToolbox\Dataproduct\TS_Images',var3);
+            print(path,'-djpeg');
+        end
     end
+    
 end
-
+end
